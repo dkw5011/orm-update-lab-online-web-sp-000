9df7ff0b3d690d294de2fcfb275492ba7d4cfee6
Danielle Bergbower <dkw5011@gmail.com>
2020-03-18 03:05:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
   
   def self.new_from_db(row)
     student = self.new
